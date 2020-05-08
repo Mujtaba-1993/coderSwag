9ca741f2d9cb87b0d1128c99171b60fee357d35f
Mujtaba-1993 <64317539+Mujtaba-1993@users.noreply.github.com>
2020-05-08 15:05:25 +0300

    leston 02

---
+++ app/src/main/java/controler/MainActivity.kt
@@ -0,0 +1,23 @@
+package controler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.mujtaba.coderswag.R
+import kotlinx.android.synthetic.main.activity_main.*
+import model.Category
+import services.DataService
+
+class MainActivity : AppCompatActivity() {
+    lateinit var adapter: ArrayAdapter<Category>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter= ArrayAdapter(this,
+        android.R.layout.simple_list_item_1,
+        DataService.categories)
+        categoryyListVeiw.adapter=adapter
+    }
+}
